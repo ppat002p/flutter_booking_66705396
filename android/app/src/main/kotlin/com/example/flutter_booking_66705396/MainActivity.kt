b499eafeb76cbda0d1e23aac7a7c600e5e62e486
ppat002p <ppat002p@gmail.com>
2026-03-17 15:58:14 -0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/flutter_booking_66705396/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_booking_66705396
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'login.dart'; //เพิ่ม
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Meeting Room Booking',
+      home: LoginPage(),   // ✅ หน้าแรกแสดงรายการห้องประชุม
+    );
+
+  }
+
+}
